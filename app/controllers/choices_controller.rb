da7d4b35d50126eb547f45dfe0fbe8fb8ac1928a
derekdapp <derekdapp@gmail.com>
2014-09-13 11:29:55 -0600

    Working on questions and coices controllers

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,16 +1,25 @@
 class ChoicesController < ApplicationController
   def create
+    @choice = Choice.new(choice_params)
   end
 
   def new
+    @choice = Choice.new
   end
 
   def show
   end
 
   def update
+    @choice = Choice.find(params[:id])
+    if @choice.update(choice_params)
+      redirect_to @choice
+    else
+      render :edit, notice: "failure"
+    end
   end
 
   def destroy
+    
   end
 end
